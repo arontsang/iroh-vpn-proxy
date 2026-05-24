53bbd2e3c86fd7f790b365d1ffa13a2f62dc7566
Aron Tsang <arontsang@gmail.com>
2026-05-24 22:29:27 +0800

    Switch to using async_executor for better drop

--- src/client.rs
+++ src/client.rs
@@ -2,6 +2,7 @@ mod support;
 
 use crate::support::get_value_from_env;
 use anyhow::Result;
+use async_executor::{LocalExecutor, Task};
 use iroh::endpoint::{presets, Connection, VarInt};
 use iroh::Endpoint;
 use iroh_tickets::endpoint::EndpointTicket;
@@ -17,14 +18,13 @@ use std::time::Duration;
 use tokio::io::copy_bidirectional;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::Mutex;
-use tokio::task::{JoinHandle, LocalSet};
 
 #[pin_project(PinnedDrop)]
 struct CopyJob<F> {
     #[pin]
     future: F,
     handler: Rc<Uplink>,
-    local: Rc<LocalSet>,
+    local_executor: Rc<LocalExecutor<'static>>,
 
     linger: Duration
 }
@@ -32,12 +32,11 @@ struct CopyJob<F> {
 struct Uplink {
     connection: Connection,
     _endpoint: Endpoint,
-    keep_alive: JoinHandle<()>,
+    _keep_alive: Task<()>,
 }
 
 impl Drop for Uplink {
     fn drop(&mut self) {
-        self.keep_alive.abort();
         self.connection.close(VarInt::default(), "Dropping Uplink".as_bytes());
         println!("dropping uplink");
     }
@@ -46,14 +45,14 @@ impl Drop for Uplink {
 #[pinned_drop]
 impl<F> PinnedDrop for CopyJob<F> {
     fn drop(self: Pin<&mut Self>) {
-        self.local.spawn_local({
+        self.local_executor.spawn({
             let handler = self.handler.clone();
             let linger = self.linger.clone();
             async move {
                 tokio::time::sleep(linger).await;
                 drop(handler);
             }
-        });
+        }).detach();
     }
 }
 
@@ -68,25 +67,7 @@ impl<F: Future<Output = Ret>, Ret> Future for CopyJob<F> {
 
 
 
-async fn build_new_connection() -> Result<Uplink> {
-    let server_base = std::env::var("SERVER_ADDR")?;
-    let ticket = reqwest::get(&format!("{}/iroh/ticket", server_base)).await?
-        .text()
-        .await?;
 
-    let endpoint = Endpoint::bind(presets::N0).await?;
-    let ticket = EndpointTicket::from_str(&ticket)?;
-    let connection = endpoint.connect(ticket, "stun-proxy".as_bytes()).await?;
-    println!("connected to server {}", server_base);
-    let keep_alive = tokio::spawn(async move {
-        loop {
-            // Continously ping the server to keep the connection alive.
-            reqwest::get(&format!("{}/keep_alive?time_out_secs=300", server_base)).await.ok();
-            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-        }
-    });
-    Ok(Uplink { connection, _endpoint: endpoint, keep_alive })
-}
 
 async fn handle_incoming(mut incoming: TcpStream, app: AppState) -> Result<()> {
     let handler = app.get_uplink().await?;
@@ -95,7 +76,7 @@ async fn handle_incoming(mut incoming: TcpStream, app: AppState) -> Result<()> {
     let mut upstream = tokio::io::join(recv, send);
     CopyJob {
         future: async move { copy_bidirectional(&mut incoming, &mut upstream).await },
-        local: app.local_set.clone(),
+        local_executor: app.local_executor.clone(),
         linger: app.linger,
         handler,
     }.await?;
@@ -106,7 +87,7 @@ async fn handle_incoming(mut incoming: TcpStream, app: AppState) -> Result<()> {
 #[derive(Clone)]
 struct AppState {
     handler_pool: Rc<Mutex<Cell<Weak<Uplink>>>>,
-    local_set: Rc<LocalSet>,
+    local_executor: Rc<LocalExecutor<'static>>,
     linger: Duration,
 }
 
@@ -119,17 +100,37 @@ impl AppState {
             return Ok(handler);
         }
 
-        let connection = build_new_connection().await?;
+        let connection = self.build_new_connection().await?;
 
         let ret = Rc::new(connection);
         handler.set(Rc::downgrade(&ret.clone()));
         Ok(ret)
     }
+
+    async fn build_new_connection(&self) -> Result<Uplink> {
+        let server_base = std::env::var("SERVER_ADDR")?;
+        let ticket = reqwest::get(&format!("{}/iroh/ticket", server_base)).await?
+            .text()
+            .await?;
+
+        let endpoint = Endpoint::bind(presets::N0).await?;
+        let ticket = EndpointTicket::from_str(&ticket)?;
+        let connection = endpoint.connect(ticket, "stun-proxy".as_bytes()).await?;
+        println!("connected to server {}", server_base);
+        let keep_alive = self.local_executor.spawn(async move {
+            loop {
+                // Continously ping the server to keep the connection alive.
+                reqwest::get(&format!("{}/keep_alive?time_out_secs=300", server_base)).await.ok();
+                tokio::time::sleep(Duration::from_secs(5)).await;
+            }
+        });
+        Ok(Uplink { connection, _endpoint: endpoint, _keep_alive: keep_alive })
+    }
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    let local_set = Rc::new(LocalSet::new());
+    let local_ex = Rc::new(LocalExecutor::new());
     let handler: Rc<Mutex<Cell<Weak<Uplink>>>> = Rc::new(Mutex::new(Cell::new(Weak::default())));
     let socket = SocketAddr::from(([0, 0, 0, 0], get_value_from_env::<u16>("PROXY_PORT").unwrap_or(0)));
     let socket = TcpListener::bind(socket).await?;
@@ -138,15 +139,20 @@ async fn main() -> Result<()> {
 
     let app = AppState {
         linger,
-        local_set: local_set.clone(),
+        local_executor: local_ex.clone(),
         handler_pool: handler,
     };
 
-    local_set.run_until(async {
+
+    local_ex.run(async {
         loop {
+            let local_ex = local_ex.clone();
             if let Ok((incoming, _)) = socket.accept().await {
                 let app = app.clone();
-                local_set.spawn_local(async { handle_incoming(incoming, app).await.ok() });
+
+                local_ex.spawn(async move {
+                    handle_incoming(incoming, app).await.ok();
+                }).detach();
             }
         }
     }).await;
