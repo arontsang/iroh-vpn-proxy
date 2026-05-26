040ad24a0ab6f21c31541edb48986d412b8b4dc0
Aron Tsang <arontsang@gmail.com>
2026-05-26 22:23:30 +0800

    Use BBR congestion

--- src/support/iroh.rs
+++ src/support/iroh.rs
@@ -1,9 +1,13 @@
+use std::sync::Arc;
 use iroh::Endpoint;
 use iroh::endpoint::{presets, QuicTransportConfig};
+use noq_proto::congestion::BbrConfig;
 use crate::support::get_value_from_env;
 
 pub async fn build_endpoint() -> anyhow::Result<Endpoint> {
     let mut quic_config = QuicTransportConfig::builder();
+    quic_config = quic_config.congestion_controller_factory(Arc::new(BbrConfig::default()));
+    
     if let Some(value) = get_value_from_env::<u32>("QUIC_RECV_WINDOW") {
         quic_config = quic_config.receive_window(value.into());
     };
@@ -13,7 +17,9 @@ pub async fn build_endpoint() -> anyhow::Result<Endpoint> {
     if let Some(value) = get_value_from_env::<u32>("QUIC_STREAM_WINDOW") {
         quic_config = quic_config.stream_receive_window(value.into());
     };
+
     let quic_config = quic_config.build();
+
 
     let mut endpoint = Endpoint::builder(presets::N0)
         .transport_config(quic_config);
