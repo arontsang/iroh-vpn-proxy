60500b3a8d09ef2fbef189f3de0cc0a66cedd2bf
Aron Tsang <arontsang@gmail.com>
2026-05-29 18:18:39 +0800

    Parameterize BBR Initial Window

--- src/support/iroh.rs
+++ src/support/iroh.rs
@@ -1,13 +1,23 @@
 use std::sync::Arc;
 use iroh::Endpoint;
 use iroh::endpoint::{presets, QuicTransportConfig};
-use noq_proto::congestion::BbrConfig;
+use noq_proto::congestion::{BbrConfig, ControllerFactory};
 use crate::support::get_value_from_env;
+
+
+fn get_congestion_controller() -> Arc<dyn ControllerFactory + Send + Sync + 'static> {
+    let mut ret = BbrConfig::default();
+    if let Some(value) = get_value_from_env("QUIC_BBR_INITIAL_WINDOW") {
+        ret.initial_window(value);
+    }
+
+    Arc::new(ret)
+}
 
 pub async fn build_endpoint() -> anyhow::Result<Endpoint> {
     let mut quic_config = QuicTransportConfig::builder();
-    quic_config = quic_config.congestion_controller_factory(Arc::new(BbrConfig::default()));
-    
+    quic_config = quic_config.congestion_controller_factory(get_congestion_controller());
+
     if let Some(value) = get_value_from_env::<u32>("QUIC_RECV_WINDOW") {
         quic_config = quic_config.receive_window(value.into());
     };
